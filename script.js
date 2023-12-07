5d08969e6245bb3a83b123327add2f2ce9cdb3b7
Shaik Mahammed Arif <129642434+shaikMahammedarif@users.noreply.github.com>
2023-12-07 22:09:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ if (userNameInpEle.value === "") {
 }
 userPassInpELe.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
-        if (userNameInpEle.value === "219x1a28472858" && userPassInpELe.value === "AAKUASHU4758") {
+        if (userNameInpEle.value === "219x1a284758" && userPassInpELe.value === "AAKUASHU4758") {
             alert("Login Successful");
             rideBtnInpEle.classList.remove("d-none");
         }
